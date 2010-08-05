97700779c7ec01b2949f5b0aff96fea75eea9a8a
Yuta Namiki <namiki@vmware-debian-etch.(none)>
2010-08-05 21:11:56 +0900

    remove city selector.

--- content/calilay.js
+++ content/calilay.js
@@ -11,14 +11,6 @@
 
 (function () {
      var appkey = '0f316d2b698c28451ed3f5f5223df15b';
-     unsafeWindow.city_selector = new CalilCitySelectDlg(
-         {appkey: appkey,
-      	  select_func: function(sysytemid_list, pref_name){
-              alert(sysytemid_list);
-	          console.log(systemid_list + ":" +  pref_name);
-	      }
-         });
-     $(document.body).append('<a href="javascript:city_selector.showDlg()">Select</a>');
      var isbnList = [];
      $('.binder_data').each(function(i) {
          var url = $('a[href^="http://www.amazon.co.jp/"]:first', this).attr('href');
